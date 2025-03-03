da9ea627860a588868e5dfb23e7c156116cc68c4
zero-trace-official <zerotrace-support@proton.me>
2025-03-03 02:21:27 -0800

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,19 +1,15 @@
 // routes/authRouter.js
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');  // Make sure this path is correct
-const { verifyToken } = require('../middleware/verifyToken');  // Make sure this path is correct
+const authController = require('../controllers/authController');
+const { verifyToken } = require('../middleware/verifyToken');
 
-// Route for rendering the login page
-router.get('/login', authController.getLogin);  // Ensure this method exists in authController
-
-// Route for handling admin login
+router.get('/login', authController.getLogin);
 router.post('/login', authController.login);
-
-// Route for rendering the change credentials page
 router.get('/change-credentials', verifyToken, authController.getChangeCredentials);
-
-// Route for handling the change of username and password
 router.post('/change-credentials', verifyToken, authController.changeCredentials);
+
+// New route for logging out from all devices
+router.post('/logout-all', verifyToken, authController.logoutAll);
 
 module.exports = router;
