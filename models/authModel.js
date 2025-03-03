b28e09adab60d2ebf1d60ab556bbd79a6b633ec2
zero-trace-official <zerotrace-support@proton.me>
2025-03-03 02:23:30 -0800

    Update authModel.js

--- models/authModel.js
+++ models/authModel.js
@@ -1,19 +1,23 @@
+// models/authModel.js
 const mongoose = require('mongoose');
 
-// Define the schema for Auth (Admin User)
 const authSchema = new mongoose.Schema({
   username: { 
     type: String, 
     required: true,
-    unique: true, // Ensure username uniqueness
+    unique: true,
     trim: true,
   },
   password: { 
     type: String, 
     required: true 
   },
-}, { timestamps: true });  // Automatically handle createdAt and updatedAt
+  // New field for token versioning
+  tokenVersion: { 
+    type: Number, 
+    default: 0 
+  },
+}, { timestamps: true });
 
 const Auth = mongoose.models.Auth || mongoose.model('Auth', authSchema);
-
 module.exports = Auth;
